f8eea6853680a050cedbf87b799912300c28742f
Vitaly Kartashov <kartashoffv@users.noreply.github.com>
2025-06-10 09:56:21 +0300

    deployments updates

--- deployments/script.sh
+++ deployments/script.sh
@@ -8,4 +8,4 @@ python -m source.db.init_db
 echo "Database initialized"
 alembic upgrade head
 echo "Database upgraded"
-python -m source.main
+python -m source.main & streamlit run source/web/app.py --server.port=8501 --server.address=0.0.0.0
